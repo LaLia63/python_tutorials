1b5c2483e345cf16906c2b16d890b7c64a0806a7
Lia <hsuyatizaw0603@gmail.com>
2026-08-17 21:51:19 +0630

    new madlibs game added

---
+++ MadlibsGame.py
@@ -0,0 +1,28 @@
+# Madlibs game
+# word game where you create a story
+# by filling in the blanks with random words
+
+print("Welcome to the Madlibs game!")
+print("Today we are going to create a story together!")
+
+adjective1 = input("Please enter an adjective (description): ")
+animal1 = input("Please enter an animal: ")
+place1 = input("Please enter a place: ")
+verb1 = input("Please enter a verb (action): ")
+food1 = input("Please enter a food: ")
+
+print(f"Yesterday, I saw a {adjective1} {animal1} at my {place1}.")
+print(f"Suddenly, it started to {verb1} while eating {food1}!")
+print(f"Everyone screamed, What is happening?!")
+
+name1 = input("Please enter a name: ")
+animal2 = input("Please enter an animal: ")
+adjective2 = input("Please enter an adjective (description): ")
+place2 = input("Please enter a place: ")
+verb2 = input("Please enter a verb (action): ")
+object1 = input("Please enter an object: ")
+
+print(f"{name1} went on a adventure with a {animal2}.")
+print(f"They discovered a {adjective2} {place2} and decided to {verb2}.")
+print(f"Inside, they found a mysterious {object1}.")
+print(f"Suddenly, the adventure became very strange...")
